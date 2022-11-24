1a960ebbc8f3bcba3ad77accf315a4e6e1ec3146
Emanuela91 <emanuela.marson91@gmail.com>
2022-11-24 06:50:06 +0100

    aggiunti commenti

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// Milestone 2
+
+// Array con oggetti
 const images = [ 
     {
         "image" : "img/01.webp", 
@@ -33,7 +36,7 @@ const images = [
 console.log(images);
 
 
-
+// Aggiungo le immagini, titolo e testo e lo faccio scorrere 
 for (let i = 0; i < images.length; i++) {
     let imagesIesimo = images[i];
     const cont = document.querySelector(".img_container");
@@ -45,10 +48,10 @@ for (let i = 0; i < images.length; i++) {
 const divArray = document.getElementsByClassName('foto');
 divArray[0].classList.add("active");
 
+
+// Creo i bottoni
 let activeItem = 0;
 
-
-// Creo il bottone
 let avanti = document.getElementById("avanti");
 let indietro = document.getElementById("indietro");
 
@@ -57,28 +60,23 @@ avanti.addEventListener("click",
         if(activeItem < divArray.length - 1){
             divArray[activeItem].classList.remove("active");
             activeItem++;
-            divArray[activeItem].classList.add("active");
-        }
-
-        else if(activeItem === divArray.length - 1);{
-            indietro.classList.add("hidden");
-        }
+            divArray[activeItem].classList.add("active"); 
+        }       
     }
-
+    
 )
 
+
+
+    
 indietro.addEventListener("click",
-    function(){
+function(){
         if(activeItem > 0){
             divArray[activeItem].classList.remove("active");
             activeItem--;
             divArray[activeItem].classList.add("active");
         }
-
-        else if(activeItem == divArray.length + 1){
-            avanti.classList.add("hidden");
-
-        }
+    
     }
 
 )
